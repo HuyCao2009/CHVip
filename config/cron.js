fe0a8130f88080e140cab45cdb0403c16f4763ba
HuyCao2009 <huydeptrainhungkhongpd@gmail.com>
2026-03-11 19:23:28 +0700

    Update cron.js

--- config/cron.js
+++ config/cron.js
@@ -1,10 +1,11 @@
-
 let CronJob = require('cron').CronJob;
 let CronHu          = require('../app/Cron/EventHu');
 let XSMB            = require('../app/Cron/XSMB/XSMB');
 let XSMB_trathuong  = require('../app/Cron/XSMB/XSMB_trathuong');
+let TaiXiu          = require('../app/Cron/TaiXiu');
 
 module.exports = function() {
+
 	new CronJob('0 0 0 * * *', function() {
 		CronHu();
 	}, null, true, 'Asia/Ho_Chi_Minh');
@@ -15,5 +16,9 @@ module.exports = function() {
 
 	new CronJob('0 35 18 * * *', function() {
 		XSMB_trathuong();
+	}, null, true, 'Asia/Ho_Chi_Minh');
+
+	new CronJob('*/60 * * * * *', function() {
+		TaiXiu();
 	}, null, true, 'Asia/Ho_Chi_Minh');
 }
