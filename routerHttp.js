4bd5090b74eaff952559dc8109cc2975d45fde71
HuyCao2009 <huydeptrainhungkhongpd@gmail.com>
2026-03-13 18:00:24 +0700

    Update routerHttp.js

--- routerHttp.js
+++ routerHttp.js
@@ -1,4 +1,3 @@
-// Router HTTP / HTTPS
 let mobile = require('is-mobile');
 let bank   = require('./routes/bank');
 
@@ -13,17 +12,17 @@ module.exports = function(app, redT) {
 		}
 	});
 
-	// Android download
+	// Android
 	app.get('/download/android', function(req, res) {
 		return res.render('download/android');
 	});
 
-	// Admin panel
+	// Admin
 	app.get('/68ClubA/', function(req, res) {
 		return res.render('admin');
 	});
 
-	// Fanpage redirect
+	// Fanpage
 	app.get('/fanpage/', function(req, res) {
 		return require('./routes/fanpage/redirect')(res);
 	});
@@ -33,31 +32,29 @@ module.exports = function(app, redT) {
 		return res.render('help/ios');
 	});
 
-	// Telegram redirect
+	// Telegram
 	app.get('/telegram/', function(req, res) {
 		return require('./routes/telegram/redirect')(res);
 	});
 
-	// Nạp thẻ callback
+	// Callback nạp thẻ
 	app.post('/c40e7445f27f71a00365b36588d60e70', function(req, res) {
-		return require('./app/Controllers/shop/nap_the_callback')(req,res);
-	});
+        return require('./app/Controllers/shop/nap_the_callback')(req,res);
+    });
 
 	// Momo callback
 	app.get('/momocallback', function(req, res) {
-		return require('./app/Controllers/shop/momocallback')(req,res);
-	});
+        return require('./app/Controllers/shop/momocallback')(req,res);
+    });
 
 	// Bank callback
 	app.get('/bankcallback', function(req, res) {
-		return require('./app/Controllers/shop/bankcallback')(req,res);
-	});
+        return require('./app/Controllers/shop/bankcallback')(req,res);
+    });
 
-	// Auto bank
 	app.get('/autobankz', function(req, res) {
-		return require('./app/Controllers/shop/autocallback')(req,res);
-	});
+        return require('./app/Controllers/shop/autocallback')(req,res);
+    });
 
-	// API routes
 	require('./routes/api')(app, redT);
 };
