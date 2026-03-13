3bf9f0a330bbcf2dc3756e0a069409e9bf730bd0
HuyCao2009 <huydeptrainhungkhongpd@gmail.com>
2026-03-13 18:08:28 +0700

    Update routerSocket.js

--- routerSocket.js
+++ routerSocket.js
@@ -1,13 +1,40 @@
-
 let users = require('./socketUsers');
 let admin = require('./socketAdmin');
-// Router Websocket
 
 module.exports = function(app, redT) {
+
 	app.ws('/client', function(ws, req) {
-		users(ws, redT);
+
+		try{
+			users(ws, redT);
+		}catch(e){
+			console.error("Socket user error:", e);
+		}
+
+		ws.on('error', function(err){
+			console.error("WS client error:", err);
+		});
+
+		ws.on('close', function(){
+			console.log("Client disconnected");
+		});
 	});
+
 	app.ws('/vn1102', function(ws, req) {
-		admin(ws, redT)
+
+		try{
+			admin(ws, redT);
+		}catch(e){
+			console.error("Socket admin error:", e);
+		}
+
+		ws.on('error', function(err){
+			console.error("WS admin error:", err);
+		});
+
+		ws.on('close', function(){
+			console.log("Admin disconnected");
+		});
 	});
+
 };
